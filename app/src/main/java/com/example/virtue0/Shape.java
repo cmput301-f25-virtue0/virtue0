279cc1acaa0c92b5fb114a68a478c06bd402b646
SpaceLobster210 <SpaceLobster188@gmail.com>
2025-09-24 18:52:35 -0600

    Add color to Shape.java

--- app/src/main/java/com/example/virtue0/Shape.java
+++ app/src/main/java/com/example/virtue0/Shape.java
@@ -3,6 +3,7 @@ package com.example.virtue0;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "purple";
 
     public int getX() {
         return x;
