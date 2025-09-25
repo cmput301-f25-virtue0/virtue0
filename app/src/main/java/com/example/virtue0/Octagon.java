29730e010bd5c9148f86dace11fde7733a3f2cb5
Felix Wu <felixwu@Felixs-MacBook-Pro.local>
2025-09-24 18:29:31 -0600

    It' s an octagon

---
+++ app/src/main/java/com/example/virtue0/Octagon.java
@@ -0,0 +1,34 @@
+package com.example.virtue0;
+
+public class Octagon extends Shape{
+    private int side0;
+    private int side1;
+    private int side2;
+    private int side3;
+    private int side4;
+    private int side5;
+    private int side6;
+    private int side7;
+
+    public Octagon(int side0, int side1, int side2, int side3, int side4, int side5, int side6, int side7) {
+        this.side0 = side0;
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        this.side4 = side4;
+        this.side5 = side5;
+        this.side6 = side6;
+        this.side7 = side7;
+    }
+
+    public Octagon() {
+        this.side0 = 0;
+        this.side1 = 0;
+        this.side2 = 0;
+        this.side3 = 0;
+        this.side4 = 0;
+        this.side5 = 0;
+        this.side6 = 0;
+        this.side7 = 0;
+    }
+}
