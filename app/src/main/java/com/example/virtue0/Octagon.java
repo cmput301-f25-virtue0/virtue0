ffa8527317aeca95b880bdf9ceb15f25467e66b2
Felix Wu <felixwu@Felixs-MacBook-Pro.local>
2025-09-24 18:33:22 -0600

    it's an octagon part 2

--- app/src/main/java/com/example/virtue0/Octagon.java
+++ app/src/main/java/com/example/virtue0/Octagon.java
@@ -31,4 +31,68 @@ public class Octagon extends Shape{
         this.side6 = 0;
         this.side7 = 0;
     }
+
+    public int getSide0() {
+        return side0;
+    }
+
+    public void setSide0(int side0) {
+        this.side0 = side0;
+    }
+
+    public int getSide1() {
+        return side1;
+    }
+
+    public void setSide1(int side1) {
+        this.side1 = side1;
+    }
+
+    public int getSide2() {
+        return side2;
+    }
+
+    public void setSide2(int side2) {
+        this.side2 = side2;
+    }
+
+    public int getSide3() {
+        return side3;
+    }
+
+    public void setSide3(int side3) {
+        this.side3 = side3;
+    }
+
+    public int getSide4() {
+        return side4;
+    }
+
+    public void setSide4(int side4) {
+        this.side4 = side4;
+    }
+
+    public int getSide5() {
+        return side5;
+    }
+
+    public void setSide5(int side5) {
+        this.side5 = side5;
+    }
+
+    public int getSide6() {
+        return side6;
+    }
+
+    public void setSide6(int side6) {
+        this.side6 = side6;
+    }
+
+    public int getSide7() {
+        return side7;
+    }
+
+    public void setSide7(int side7) {
+        this.side7 = side7;
+    }
 }
